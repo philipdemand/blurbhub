8b3e1b01ea32017488c120af7c5e76d0f07753e9
Philip Demand <philipdemand@yahoo.com>
2024-06-23 17:32:14 -0400

    posts avail

--- client/src/App.js
+++ client/src/App.js
@@ -19,6 +19,7 @@ const App = () => {
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
+          <Route path="/posts" element={<PostsContainer />} />
         </Routes>
       ) : (
         <Routes>
